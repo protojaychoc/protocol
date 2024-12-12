7dee9e66af68711c70eebf88462fa58d9cb0e231
JRigaldo <rigaldo.jeremy@gmail.com>
2024-12-12 13:28:59 +0100

    add animation menu

--- src/contexts/MenuContext.tsx
+++ src/contexts/MenuContext.tsx
@@ -1,47 +1,93 @@
 "use client";
-import { createContext, useContext, useState, useEffect, ReactNode, use } from "react";
+
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+  use,
+} from "react";
 
 interface MenuContextProps {
-    activeMenuDesktop: string | null;
-    activeMenuMobile: boolean;
-    toggleMenuDesktop: (menuId: string) => void;
-    toggleMenuMobile: () => void;
-    closAllMenus: () => void;
+  activeMenuDesktop: string | null;
+  activeMenuMobile: boolean;
+  toggleMenuDesktop: (menuLink: string) => void;
+  toggleMenuMobile: () => void;
+  closeAllMenus: () => void;
+  isDisplay: boolean;
+  isVisible: boolean;
 }
 
 const MenuContext = createContext<MenuContextProps | undefined>(undefined);
 
 export const MenuProvider = ({ children }: { children: ReactNode }) => {
-    const [activeMenuDesktop, setActiveMenuDesktop] = useState<string | null>(null);
-    const [activeMenuMobile, setActiveMenuMobile] = useState(false);
+  const [activeMenuDesktop, setActiveMenuDesktop] = useState<string | null>(
+    null
+  );
+  const [activeMenuMobile, setActiveMenuMobile] = useState(false);
 
-    const toggleMenuDesktop = (menuId: string) => {
-        setActiveMenuDesktop((prev) => (prev === menuId ? null : menuId));
-        setActiveMenuMobile(false);
-    };
+  const [isVisible, setIsVisible] = useState(false);
+  const [isDisplay, setIsDisplay] = useState(false);
 
-    const toggleMenuMobile = () => {
-        setActiveMenuMobile((prev) => !prev);
-        setActiveMenuDesktop(null);
+  const showMenu = () => {
+    setTimeout(() => {
+        setIsDisplay(true); // Affiche l'élément en `display: block`
+        setIsVisible(true); // Déclenche l'animation
+      }, 50);
+    setActiveMenuMobile(true);
+  };
+
+  const hideMenu = () => {
+    setIsVisible(false); // Lance le fade-out
+    setTimeout(() => {
+      setIsDisplay(false); // Cache complètement après l'animation
+      setActiveMenuMobile(false);
+    }, 480);
+  };
+
+  const toggleMenuMobile = () => {
+    if (activeMenuMobile) {
+      hideMenu();
+    } else {
+      showMenu();
     }
+  };
 
-    const closAllMenus = () => {
-        setActiveMenuDesktop(null);
-        setActiveMenuMobile(false);
+  const toggleMenuDesktop = (menuLink: string) => {
+    setActiveMenuDesktop((prev) => (prev === menuLink ? null : menuLink));
+    if (!activeMenuDesktop) {
+      hideMenu();
     }
+  };
 
-    return (
-        <MenuContext.Provider value={{ activeMenuDesktop, activeMenuMobile, toggleMenuDesktop, toggleMenuMobile, closAllMenus }}>
-            {children}
-        </MenuContext.Provider>
-    );
+  const closeAllMenus = () => {
+    setActiveMenuDesktop(null);
+    setActiveMenuMobile(false);
+  };
+
+  return (
+    <MenuContext.Provider
+      value={{
+        activeMenuDesktop,
+        activeMenuMobile,
+        toggleMenuDesktop,
+        toggleMenuMobile,
+        closeAllMenus,
+        isDisplay,
+        isVisible
+      }}
+    >
+      {children}
+    </MenuContext.Provider>
+  );
 };
 
 export const useMenu = () => {
-    const context = useContext(MenuContext);
-    if(!context){
-        throw new Error("useMenu must be used within a MenuProvider");
-    }
+  const context = useContext(MenuContext);
+  if (!context) {
+    throw new Error("useMenu must be used within a MenuProvider");
+  }
 
-    return context;
-}
+  return context;
+};
